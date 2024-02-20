d0379b22aa3a2f34e28a0dd572b756e67a4ea814
viliamnovicky <viliamnovicky@gmail.com>
2024-02-20 20:57:34 +0100

    firebase

--- src/services/apiPortfolio.js
+++ src/services/apiPortfolio.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/database';
 import { collection, getDocs } from "firebase/firestore/lite";
 import { database } from "../utils/firebase";
 
@@ -8,3 +11,28 @@ export async function getPortfolio() {
     const portfolioList = portfolioData.docs.map(doc => doc.data());
     return portfolioList;
   }
+
+export async function uploadPortfolioImage(image, databaseName) {
+  const storageRef = firebase.storage().ref();
+  const imagesRef = storageRef.child(`portfolio/${databaseName}/${image.name}`);
+
+  try {
+    // Upload the file to Firebase Storage
+    const snapshot = await imagesRef.put(image);
+    console.log('Image uploaded successfully');
+
+    // Get the download URL of the uploaded image
+    const url = await imagesRef.getDownloadURL();
+
+    // Add the image URL to the Firebase Realtime Database
+    const database = firebase.database();
+    const imageRef = database.ref(`portfolio/${databaseName}/${image.name}`).push();
+    await imageRef.set({
+      name: image.name,
+      url: url
+    });
+    console.log('Image URL added to the database successfully');
+  } catch (error) {
+    console.error('Error uploading image or adding URL to the database:', error);
+  }
+}
